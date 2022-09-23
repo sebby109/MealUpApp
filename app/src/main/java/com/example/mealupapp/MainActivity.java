00f1d08ef4dbed5de9ffc5cf67034338893adcc7
rmanqueroochoa <rmanqueroochoa@email.arizona.edu>
2022-09-22 20:13:29 -0700

    There was a comment added

--- app/src/main/java/com/example/mealupapp/MainActivity.java
+++ app/src/main/java/com/example/mealupapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //This is a comment
         RecipeApi x = new RecipeApi();
         Log.i("test", String.valueOf(x.getI()));
     }
