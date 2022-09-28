485e5b42f3f9317633363855aebb83a2c853fba0
Sebastian <sebastianga14@hotmail.com>
2022-09-27 23:52:51 -0700

    added dependencies, internet permissions, and API class

--- app/src/main/java/com/example/mealupapp/RecipeApi.java
+++ app/src/main/java/com/example/mealupapp/RecipeApi.java
@@ -1,22 +1,63 @@
 package com.example.mealupapp;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class RecipeApi {
-    private static int i = 1;
-    private int k = 0;
+    private final OkHttpClient client = new OkHttpClient();
 
     RecipeApi(){
 
     }
 
-    public int getI(){
-        return i;
+    /*
+    * This method uses an Asynchronous get
+    * to retrieve a JSON string from
+    * the api query. It then converts that to a string.
+    * The caller sends in what they want searched (srch)
+    * and method will return results.
+    *
+    * params
+    * srch, String
+    *
+    * return
+    * None
+    * */
+    public void getAPI(String srch){
+
+        Request request = new Request.Builder()
+                .url("https://edamam-recipe-search.p.rapidapi.com/search?q=" + srch)
+                .get()
+                .addHeader("X-RapidAPI-Key", "a122f83c62msh9cc7775d532a038p19b977jsn86ba7194c4da")
+                .addHeader("X-RapidAPI-Host", "edamam-recipe-search.p.rapidapi.com")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+
+                if(response.isSuccessful()){
+                    ResponseBody responseBody = response.body();
+                    Log.d("tester", "onResponse: " + responseBody.string());
+                }
+            }
+        });
+
     }
 
-    public int addI(){
-        i += 1;
-        return i;
-    }
 }
