804b5aecd5fdb1e0690b58a49dd4887ba55a07e6
Sebastian <sebastianga14@hotmail.com>
2022-10-05 10:24:15 -0700

    made second activity screen for the recipe search

--- app/src/main/java/com/example/mealupapp/MainActivity.java
+++ app/src/main/java/com/example/mealupapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.mealupapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.util.Log;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,14 +18,27 @@ public class MainActivity extends AppCompatActivity {
 
         //This is a comment
         //There is a comment here too. hi
-        RecipeApi x = new RecipeApi();
+        //RecipeApi x = new RecipeApi();
         //SearchResults r = x.getFeed("tuna, lettuce");
         //String name = r.getNames().get(0);
         //String img = r.getImages().get(0);
         //String id = r.getIds().get(0);
         //x.findRecipe(id, img, name);
-        SearchResults feed = x.getFeed("chicken soup", 1);
 
+        //RecipeApi x = new RecipeApi();
+        //SearchResults feed = x.getFeed("chicken soup", 1);
 
+        button = (Button) findViewById(R.id.recipeButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSearchBarActivity();
+            }
+        });
+    }
+
+    public void openSearchBarActivity(){
+        Intent intent = new Intent(this, SearchBarActivity.class);
+        startActivity(intent);
     }
 }
